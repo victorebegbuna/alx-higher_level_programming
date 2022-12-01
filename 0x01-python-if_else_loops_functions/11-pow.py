83058500c816aca5bcfe57d36b3a1c5035b94616
Victor <victor4real2000ng@hotmail.com>
2022-12-01 03:43:44 +0000

    if else loops

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,8 +1,4 @@
-#!/usr/bin/env python3
-pow = __import__('11-pow').pow
+#!/usr/bin/python3
 
-print(pow(2, 2))
-print(pow(98, 2))
-print(pow(98, 0))
-print(pow(100, -2))
-print(pow(-4, 5))
+def pow(a, b):
+    return (a ** b)
